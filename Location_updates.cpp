1587002e4781eac60efeff4206c284012094e328
Atharv-s <61531703+Atharv-s@users.noreply.github.com>
2026-07-28 10:55:40 +0530

    Modify location update details and add building info
    Updated location details including place, description, map URL, and update time. Added building information.

---
+++ Location_updates.cpp
@@ -0,0 +1,25 @@
+//25 July 2026 Awfis 3rd floor GK mall above pantaloons Pimple saudagar Maharashtra 411027
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+    string place = "Spot 18 mall entrance or first floor";
+    string description = "Spot 18 mall entrance or first floor";
+    string mapUrl = "https://osm.org/go/y2Q~gWVfd?way=360242727";
+    string updateTime = "28-07-2026 6:00 PM IST";
+
+    cout << "📍 Location Update" << endl;
+    cout << "--------------------------" << endl;
+    cout << "Place: " << place << endl;
+    cout << "Description: " << description << endl;
+    cout << "Time: " << updateTime << endl;
+    cout << "Map Link: " << mapUrl << endl;
+cout << "building 	commercial
+building:levels 	6
+name 	Spot 18";
+
+    return 0;
+}
